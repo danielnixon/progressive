37004823a5ea99cb236350105b2a3ad43e503d76
Daniel Nixon <dan.nixon@gmail.com>
2016-10-29 13:20:58 +1100

    Feature detection.

--- client/src/main/scala/org/danielnixon/progressive/Progressive.scala
+++ client/src/main/scala/org/danielnixon/progressive/Progressive.scala
@@ -10,32 +10,37 @@ class Progressive {
     eventHandlers: EventHandlers
   ): Unit = {
 
-    val historyService = new HistoryService(dom.window)
-    val ajaxService = new AjaxService
-    val eventHandlerSetupService = new EventHandlerSetupService(eventHandlers.additionalSetupInitial, eventHandlers.additionalSetup)
-    val refreshService = new RefreshService(Global.virtualDom, Global.vdomParser, eventHandlerSetupService, ajaxService, eventHandlers.applyDiff)
     val userAgentService = new UserAgentService(dom.window)
-    val formSerializer = new FormSerializer
 
-    val hijaxService = new HijaxService(
-      dom.window,
-      new QueryStringService(formSerializer, eventHandlers.includeInQueryString),
-      historyService,
-      userAgentService,
-      new TransitionsService(dom.window, elements.announcementsElement, elements.errorElement, new AnimationService, views),
-      new FocusManagementService(dom.window, eventHandlers.scrollOffset _, userAgentService),
-      refreshService,
-      new EnableDisableService,
-      ajaxService,
-      eventHandlerSetupService,
-      formSerializer,
-      eventHandlers.preFormSubmit,
-      eventHandlers.postFormSubmit
-    )
+    if (userAgentService.meetsRequirements) {
 
-    historyService.initializeHistory()
-    dom.window.onpopstate = historyService.onPopState _
+      val historyService = new HistoryService(dom.window)
+      val ajaxService = new AjaxService
+      val eventHandlerSetupService = new EventHandlerSetupService(eventHandlers.additionalSetupInitial, eventHandlers.additionalSetup)
+      val refreshService = new RefreshService(Global.virtualDom, Global.vdomParser, eventHandlerSetupService, ajaxService, eventHandlers.applyDiff)
 
-    eventHandlerSetupService.setupInitial(elements.body, refreshService, hijaxService)
+      val formSerializer = new FormSerializer
+
+      val hijaxService = new HijaxService(
+        dom.window,
+        new QueryStringService(formSerializer, eventHandlers.includeInQueryString),
+        historyService,
+        userAgentService,
+        new TransitionsService(dom.window, elements.announcementsElement, elements.errorElement, new AnimationService, views),
+        new FocusManagementService(dom.window, eventHandlers.scrollOffset _, userAgentService),
+        refreshService,
+        new EnableDisableService,
+        ajaxService,
+        eventHandlerSetupService,
+        formSerializer,
+        eventHandlers.preFormSubmit,
+        eventHandlers.postFormSubmit
+      )
+
+      historyService.initializeHistory()
+      dom.window.onpopstate = historyService.onPopState _
+
+      eventHandlerSetupService.setupInitial(elements.body, refreshService, hijaxService)
+    }
   }
 }
--- client/src/main/scala/org/danielnixon/progressive/services/UserAgentService.scala
+++ client/src/main/scala/org/danielnixon/progressive/services/UserAgentService.scala
@@ -1,7 +1,27 @@
 package org.danielnixon.progressive.services
 
+import org.danielnixon.progressive.shared.Wart
 import org.scalajs.dom.Window
 
+import scala.scalajs.js
+
 class UserAgentService(window: Window) {
-  val isTouchDevice = window.hasOwnProperty("ontouchstart")
+
+  val isTouchDevice: Boolean = isDefined(window.document.documentElement, "ontouchstart")
+
+  /**
+    * True if the current user agent meets Progressive's requirements, false otherwise.
+    */
+  val meetsRequirements: Boolean = {
+    isDefined(window, "WeakMap") &&
+      isDefined(window, "history") &&
+      isDefined(window.history, "pushState") &&
+      isDefined(window.document, "querySelector") &&
+      isDefined(window.document.body, "matches")
+  }
+
+  @SuppressWarnings(Array(Wart.AsInstanceOf))
+  private def isDefined(obj: js.Object, prop: String): Boolean = {
+    !js.isUndefined(obj.asInstanceOf[js.Dynamic](prop))
+  }
 }
