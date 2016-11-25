0d140fa691efc92de830711118ae9d6389ab54c7
Daniel Nixon <dan.nixon@gmail.com>
2016-11-25 15:56:56 +1100

    Only redirect to same host.

--- server-play/src/main/scala/org/danielnixon/progressive/play/Results.scala
+++ server-play/src/main/scala/org/danielnixon/progressive/play/Results.scala
@@ -1,11 +1,20 @@
 package org.danielnixon.progressive.play
 
+import java.net.URI
+
 import org.danielnixon.progressive.play.extensions.RequestHeaderWrapper
 import play.api.mvc.{ Call, RequestHeader, Result }
 import play.api.mvc.Results.Redirect
 
+import scala.util.Try
+import scalaz.Scalaz._
+
 object Results {
   def redirectToRefererOrElse(call: Call)(implicit request: RequestHeader): Result = {
-    request.referer.map(Redirect(_)).getOrElse(Redirect(call))
+    request.referer.
+      flatMap(r => Try(new URI(r)).toOption).
+      filter(r => !r.isAbsolute || request.domain === r.getHost).
+      map(r => Redirect(r.toString)).
+      getOrElse(Redirect(call))
   }
 }
